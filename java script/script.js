315d6d0ba2adb3615bbfd154aa9de5262942c682
shrutidhanani59-sketch <shrutidhanani59@gmail.com>
2026-07-27 11:54:29 +0530

    Update script.js

--- java script/script.js
+++ java script/script.js
@@ -273,8 +273,8 @@ document.body.addEventListener('click', function(e){
    console.log(cartfilter);   
 });
 
-document.querySelector(".cart").onclick = function () {
+document.querySelector(".crt").onclick = function () {
     location.href = "cart.html";
-}
+};
 
 getproduct();
